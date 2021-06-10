321cdbda20f000bdf54a88361b0aa491ed217549
t.zemmari <tarik.zemmari@xabec.es>
2021-06-10 09:32:00 +0200

    aun no puedo deveolver videos de un usuario mediante la relacion

--- database.sql
+++ database.sql
@@ -1,27 +1,28 @@
 CREATE DATABASE IF NOT EXISTS mi_api_rest_symfony;
 USE mi_api_rest_symfony;
 
-CREATE TABLE users(
-    id       int(255) auto_increment not null,
-    name     varchar(150) not null,
+CREATE TABLE user(
+    id       bigint(255) AUTO_INCREMENT NOT NULL,
+    name     varchar(150) NOT NULL,
     lastname varchar(255),
-    email    varchar(255) not null,
-    password varchar(255) not null,
+    email    varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
     role     varchar(255),
     created_at datetime DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT pk_users PRIMARY KEY(id)
-)ENGINE=InnoDb;
+    PRIMARY KEY(id)
+)ENGINE=InnoDb AUTO_INCREMENT=1;
 
-CREATE TABLE  videos(
-    id       int(255) auto_increment not null,
-    user_id  int(255) not null,
-    title    varchar(255) not null,
+CREATE TABLE  video(
+    id       bigint(255) AUTO_INCREMENT NOT NULL,
+    user_id  bigint(255) NOT NULL,
+    title    varchar(255) NOT NULL,
     description  text,
-    url    varchar(255) not null,
+    url    varchar(255) NOT NULL,
     status varchar(50),
     created_at datetime DEFAULT CURRENT_TIMESTAMP,
     updated_at datetime DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT pk_videos PRIMARY KEY(id),
-    CONSTRAINT fk_video_user FOREIGN KEY(user_id) REFERENCES users(id)
-)ENGINE=InnoDb;
+    PRIMARY KEY(id),
+   
+    CONSTRAINT fk_video_user FOREIGN KEY(user_id) REFERENCES  user(id)
+)ENGINE=InnoDb AUTO_INCREMENT=1;
 
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,13 +4,31 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use App\Entity\User;
+use App\Entity\Video;
 
 
 class UserController extends AbstractController
 {
    
-    public function index(): Response
+    public function index()
     {
+
+       $user_repo = $this->getDoctrine()->getRepository(User::class);
+       $video_repo = $this->getDoctrine()->getRepository(Video::class);
+
+       $users = $user_repo->findAll();
+
+       foreach($users as $user){
+           echo "<h1>{$user->getName()} {$user->getLastname()}</h1>";
+       };
+       foreach($user->getVideos() as $video){
+        echo "<p>{$video->getUrl()}</p>";
+
+       }
+    
+       die();
+
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/UserController.php',
