20d60eb74cc5c472f469ca3272318c78d5ffb478
t.zemmari <tarik.zemmari@xabec.es>
2021-06-11 12:45:06 +0200

    terminada la logica de creacion de un video

--- src/Controller/VideoController.php
+++ src/Controller/VideoController.php
@@ -2,10 +2,16 @@
 
 namespace App\Controller;
 
-use App\Services\JwtAuth;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Entity\User;
+use App\Entity\Video;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\Encoder\JsonEncode;
+use Symfony\Component\Validator\Validation;
+use App\Services\JwtAuth;
+
 
 
 class VideoController extends AbstractController
@@ -33,7 +39,7 @@ class VideoController extends AbstractController
         //Devolver la respuesta
         return $response;
     }
-   
+
     public function index(): Response
     {
         return $this->json([
@@ -44,23 +50,125 @@ class VideoController extends AbstractController
 
 
 
-    public function create(Request $request,JwtAuth $jwtAuth){
+    public function create(Request $request, JwtAuth $jwtAuth)
+    {
 
 
-       $data =[
-
-        "Status" => "Error",
-        "Code"=>500,
-        "Message"=>"Video no Guardado"
-       ];
+        $token = $request->headers->get('Autorization', null);
 
 
+        $auth = $jwtAuth->authToken($token);
+
+        $data = [
+
+            "Status" => "Error",
+            "Code" => 500,
+            "Message" => "Video no Guardado"
+        ];
 
 
-       return $this->resJson($data);
+        if ($auth) {
 
+            $json = $request->get('json', null);
+            $params = json_decode($json);
+
+            $em = $this->getDoctrine()->getManager();
+            $identity = $jwtAuth->authToken($token, true);
+
+            
+
+
+            if (!empty($json)) {
+
+
+                $user = (!empty($identity->sub)) ? $identity->sub : null;
+                $title = (!empty($params->title)) ? $params->title : null;
+                $url = (!empty($params->url)) ? $params->url : null;
+                $descripction = (!empty($params->descripction)) ? $params->descripction : null;
+                $status = (!empty($params->status)) ? $params->status : null;
+
+
+                if (!empty($title) && !empty($url)) {
+
+
+                    $data = [
+
+                        "Status" => "Success",
+                        "code" => "200",
+                        "message" => "Validacion correcta",
+
+                    ];
+                } else {
+                    $data = [
+
+                        "Status" => "Error",
+                        "code" => "500",
+                        "message" => "Validacion Incorrecta",
+
+                    ];
+                }
+
+                $user_repo = $this->getDoctrine()->getRepository(User::class);
+                $user = $user_repo->findOneBy([
+                    "id"=>$identity->sub
+                ]);
+
+                $video = new Video();
+
+                $video->setTitle($title);
+                $video->setUrl($url);
+                $video->setUser($user);
+                $video->setStatus($status);
+                $video->setDescription($descripction);
+
+                $createdAt = new \DateTime('now');
+                $updatedAt = new \DateTime('now');
+
+                $video->setCreatedAt($createdAt);
+                $video->setUpdatedAt($updatedAt);
+
+                $video_repo = $this->getDoctrine()->getRepository(Video::class);
+
+
+                $isset_video = $video_repo->findBy(array(
+                    'url' => $url
+                ));
+
+
+                if (count($isset_video) == 0) {
+                    $data = [
+        
+                        "Status" => "Success",
+                        "code" => "200",
+                        "message" => "Video Creado Correctamente",
+                        "user" => $video
+        
+                    ];
+                } else {
+                    $data = [
+        
+                        "Status" => "Error",
+                        "code" => "500",
+                        "message" => "El Video ya existe"
+        
+                    ];
+                }
+        
+
+
+                $em->persist($video);
+                $em->flush();
+
+
+            }
+        }
+
+
+       
+
+
+
+
+        return $this->resJson($data);
     }
 }
-
-
-
