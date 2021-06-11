681849609bd59d01d679e9d4353a54b8be8b7066
t.zemmari <tarik.zemmari@xabec.es>
2021-06-11 12:13:08 +0200

    compordaba la ruta de crear videos si funciona y recibe datos, ahora hay que crear el objeto json

--- src/Controller/VideoController.php
+++ src/Controller/VideoController.php
@@ -2,12 +2,37 @@
 
 namespace App\Controller;
 
+use App\Services\JwtAuth;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 
 class VideoController extends AbstractController
 {
+
+    private function resJson($data)
+    {
+
+        //serializar datos con servicio serializer
+
+        $json = $this->get('serializer')->serialize($data, 'json');
+
+        //Response con httpfoudation
+
+        $response = new Response();
+
+        // Asignar contenido a la respuesta
+
+        $response->setContent($json);
+
+        //Indicar formato de la respuesta
+
+        $response->headers->set('content-Type', 'application/json');
+
+        //Devolver la respuesta
+        return $response;
+    }
    
     public function index(): Response
     {
@@ -16,4 +41,26 @@ class VideoController extends AbstractController
             'path' => 'src/Controller/VideoController.php',
         ]);
     }
+
+
+
+    public function create(Request $request,JwtAuth $jwtAuth){
+
+
+       $data =[
+
+        "Status" => "Error",
+        "Code"=>500,
+        "Message"=>"Video no Guardado"
+       ];
+
+
+
+
+       return $this->resJson($data);
+
+    }
 }
+
+
+
